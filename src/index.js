6ff0fe20934f6ac8e76bef759db8b63fbf388ab9
lubien <lubien1996@gmail.com>
2018-05-29 00:29:50 -0300

    Parse markdown by myself

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import * as repo from './repo'
 import * as commit from './commit'
 import * as markdown from './markdown'
 
-import type MarkdownItT from 'markdown-it'
-import type Token from 'markdown-it/lib/token'
 import type { GitHubRepository } from './repo'
 
 const awesome : GitHubRepository = {
@@ -16,8 +14,7 @@ const awesome : GitHubRepository = {
 repo.getRepository(awesome)
   .then(repo => repo.getHeadCommit())
   .then(commit.getReadme)
-  .then(markdown.parseMarkdown)
-  .then(markdown.getLists)
+  .then(markdown.getCategories)
   .then(t => {
     console.log(t)
   })
--- src/markdown.js
+++ src/markdown.js
@@ -1,174 +1,86 @@
 // @flow
 
-import MarkdownIt from 'markdown-it'
+type CategoryMap = { [string]: Category }
 
-import type Token from 'markdown-it/lib/token'
+export function getCategories (rawMarkdown : string) : CategoryMap {
+  const categories : CategoryMap = {}
 
-type ListMap = { [string]: BulletList }
+  const lines : string[] = rawMarkdown.split('\n')
+  let currentHeading : string = ''
+  let links : Link[] = []
 
-const TOKEN_HEADING_OPEN = 'heading_open'
-const TOKEN_BULLET_LIST_OPEN = 'bullet_list_open'
-const TOKEN_BULLET_LIST_CLOSE = 'bullet_list_close'
-const TOKEN_LIST_ITEM_OPEN = 'list_item_open'
-const TOKEN_LIST_ITEM_CLOSE = 'list_item_close'
-const TOKEN_PARAGRAPH_OPEN = 'paragraph_open'
-const TOKEN_PARAGRAPH_CLOSE = 'paragraph_close'
-const TOKEN_INLINE = 'inline'
-const TOKEN_LINK_OPEN = 'link_open'
-const TOKEN_LINK_CLOSE = 'link_close'
-const TOKEN_TEXT = 'text'
+  while (lines.length) {
+    const currentLine = lines.shift()
 
-const markdownIt = new MarkdownIt()
+    if (
+      (isHeading(currentLine) || !lines.length) && 
+      currentHeading !== '' &&
+      links.length
+    ) {
+      categories[currentHeading] = new Category(currentHeading, links)
+      currentHeading = ''
+      links = []
+    }
 
-export function parseMarkdown (src: string) : Token[] {
-  return markdownIt.parse(src)
-}
-
-export function getLists (tokens: Token[]) {
-  return getBulletLists(new Tokens(tokens))
-}
-
-export function getBulletLists (tokens : Tokens) : ListMap {
-  const lists : ListMap = {}
-  let currentHeading = ''
-
-  while (tokens.length) {
-    const token = tokens.consume()
-
-    if (token.type === TOKEN_HEADING_OPEN) {
-      const inlineToken = tokens.consume()
-
-      if (inlineToken.type !== TOKEN_INLINE) {
-        throw new Error('Heading open token isn\'t proceded by inline')
+    if (isHeading(currentLine)) {
+      currentHeading = getHeadingTitle(currentLine)
+    } else if (isListItem(currentLine)) {
+      if (isNested(currentLine) && links.length) {
+        const lastUnnestedLink = links[links.length - 1]
+        lastUnnestedLink.children.push(getListItemLink(currentLine))
+      } else {
+        links.push(getListItemLink(currentLine))
       }
-
-      currentHeading = inlineToken.content
-    } else if (token.type === TOKEN_BULLET_LIST_OPEN) {
-      lists[currentHeading] = consumeBulletList(tokens)
     }
   }
 
-  return lists
+  return categories
 }
 
-export function consumeBulletList (tokens : Tokens) : BulletList {
-  const children = []
+/*
+ * listItemRegex
+ *
+ * Examples:
+ *    Complete:
+ *      `- [Recursion Schemes](https://github.com/passy/awesome-recursion-schemes) - Traversing nested data structures.`
+ *
+ *    No description:
+ *      `- [Math](https://github.com/rossant/awesome-math)`
+ *
+ *    Nested:
+ *      `	- [Browser Extensions](https://github.com/stefanbuck/awesome-browser-extensions-for-github)`
+ */
+const listItemRegex = /^\s*-\s+\[(.*)\]\((.*)\)(.+)?$/
 
-  while (tokens.length) {
-    const token = tokens.consume()
-
-    if (token.type === TOKEN_BULLET_LIST_CLOSE) {
-      break
-    } else if (token.type === TOKEN_BULLET_LIST_OPEN) {
-      children.push(consumeBulletList(tokens))
-    } else if (token.type === TOKEN_LIST_ITEM_OPEN) {
-      children.push(consumeListItem(tokens))
-    } else {
-      children.push(token)
-    }
-  }
-
-  return new BulletList(children)
+function isHeading (line: string) : boolean {
+  return line.trim().startsWith('#')
 }
 
-export function consumeListItem (tokens : Tokens) : ListItem {
-  const children = []
-
-  while (tokens.length) {
-    const token = tokens.consume()
-
-    if (token.type === TOKEN_LIST_ITEM_CLOSE) {
-      break
-    } else if (token.type === TOKEN_PARAGRAPH_OPEN) {
-      children.push(...consumeParagraph(tokens))
-    } else if (token.type === TOKEN_BULLET_LIST_OPEN) {
-      children.push(consumeBulletList(tokens))
-    } else {
-      children.push(token)
-    }
-  }
-
-  return new ListItem(children)
+function isListItem (line: string) : boolean {
+  return listItemRegex.test(line)
 }
 
-export function consumeParagraph (tokens : Tokens) : mixed[] {
-  const children = []
-
-  while (tokens.length) {
-    const token = tokens.consume()
-
-    if (token.type === TOKEN_PARAGRAPH_CLOSE) {
-      break
-    } else if (token.type === TOKEN_INLINE) {
-      children.push(consumeInline(token))
-    } else {
-      children.push(token)
-    }
-  }
-
-  return children
+function isNested (line: string) : boolean {
+  return /^\s+/.test(line)
 }
 
-export function consumeInline (token : Token) {
-  if (token.children.length === 3 || token.children.length === 4) {
-    const [
-      linkOpenToken,
-      textToken,
-      linkCloseToken,
-      descriptionToken
-    ] = token.children
-
-    if ([
-      [linkOpenToken, TOKEN_LINK_OPEN],
-      [textToken, TOKEN_TEXT],
-      [linkCloseToken, TOKEN_LINK_CLOSE]
-    ].some(([token, expectedType]) => token.type !== expectedType)) {
-      throw new Error('Unexpected token type')
-    }
-
-    return new Link(textToken.content, linkOpenToken.attrGet('href'), descriptionToken && descriptionToken.content || '')
-  }
+function getHeadingTitle (line : string) : string {
+  const match = /#+\s+(.*)/.exec(line)
+  return match[1]
 }
 
-
-class Tokens {
-  idx : number = 0 
-  tokens : Token[] = []
-
-  constructor (tokens : Token[]) {
-    this.tokens = tokens
-  }
-
-  get length () : number {
-    return this.tokens.length - this.idx
-  }
-
-  get current () : Token {
-    return this.tokens[this.idx]
-  }
-
-  peek (next = 1) : Token {
-    return this.tokens[this.idx + next]
-  }
-
-  consume () : Token  {
-    return this.tokens[this.idx++]
-  }
+function getListItemLink (line : string) : Link {
+  const [_, label, link, description] = listItemRegex.exec(line)
+  return new Link(label, link, description)
 }
 
-class BulletList {
-  children = []
+class Category {
+  name : string = ''
+  links : Link[] = []
 
-  constructor (children) {
-    this.children = children
-  }
-}
-
-class ListItem {
-  children = []
-
-  constructor (children) {
-    this.children = children
+  constructor (name: string, children) {
+    this.name = name
+    this.links = children
   }
 }
 
@@ -176,10 +88,17 @@ class Link {
   label : string = ''
   url : string = ''
   description : string = ''
+  children : Link[] = []
 
   constructor (label: string, url: string, description: string) {
     this.label = label
     this.url = url
-    this.description = description
+    /*
+     * In awesome lists we often see this format:
+     *    `- [Label](Link) - Description`
+     * The token for description comes with ` - Description` (if present at all).
+     * So I just remove ` - ` at the start.
+     */
+    this.description = description ? description.replace(/^(\s*-\s*)/, '') : ''
   }
 }
